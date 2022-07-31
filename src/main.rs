898516514d3ab9d5629607e787b37fba3fbbbf42
jtabalon <joseph@tabalon.io>
2022-07-31 09:55:36 -0700

    generate random selection from computer

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,25 @@ use rand::Rng;
 
 
 
-fn get_computer_choice() -> i32 {
-    let num = rand::thread_rng().gen_range(0..3);
-    num
+fn get_computer_choice() -> &'static str {
+    let num: i32 = rand::thread_rng().gen_range(0..3);
 
+    // let hand: &str = match num {
+    //     0 => String::from("Rock"),
+    //     1 => String::from("Paper"),
+    //     2 => String::from("Scissors"),
+    // };
 
-
+    let hand: &str = match num {
+        0 => "Rock",
+        1 => "Paper",
+        2 => "Scissors",
+        _ => "Rock"
+    };
+    hand
 }
 
-
-
-
-
-
-
-
-
 fn main() {
-    let number = get_computer_choice();
-    println!("{}", number);
+    let hand = get_computer_choice();
+    println!("{}", hand);
 }
